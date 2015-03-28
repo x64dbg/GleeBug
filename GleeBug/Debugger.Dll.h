cd6b15fd8c118caac01dbe47c71627bde1072898
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-03-28 01:37:49 +0100

    initial structure

---
+++ GleeBug/Debugger.Dll.h
@@ -0,0 +1,17 @@
+#ifndef _DEBUGGER_DLL_H
+#define _DEBUGGER_DLL_H
+
+#include "_global.h"
+
+namespace GleeBug
+{
+	/**
+	\brief DLL information structure.
+	*/
+	struct DllInfo
+	{
+
+	};
+};
+
+#endif //_DEBUGGER_DLL_H
