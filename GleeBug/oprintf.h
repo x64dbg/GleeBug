7343c99cfa74d7f31ff53e365787176c2d081a4e
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2017-12-29 17:13:13 +0100

    add debug print functionality

---
+++ GleeBug/oprintf.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <windows.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+static inline void oprintf(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    static char dprintf_msg[66000];
+    *dprintf_msg = 0;
+    vsnprintf_s(dprintf_msg, sizeof(dprintf_msg), format, args);
+    if(GetConsoleWindow() == NULL)
+        AllocConsole();
+    auto hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD w;
+    WriteFile(hOut, dprintf_msg, strlen(dprintf_msg), &w, nullptr);
+}
+
+static inline void oputs(const char* text)
+{
+    oprintf("%s\n", text);
+}
+
+static inline void oprintf_args(_In_z_ _Printf_format_string_ const char* Format, va_list Args)
+{
+    char buffer[16384];
+    vsnprintf_s(buffer, _TRUNCATE, Format, Args);
+    oprintf("%s", buffer);
+}
