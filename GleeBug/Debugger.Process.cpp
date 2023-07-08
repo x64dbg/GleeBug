a9a339ad456f2154db2c468c3fb9b7131cc88ee0
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2023-07-08 14:12:28 +0200

    Switch to an amalgamated Zydis 4.0.0

--- GleeBug/Debugger.Process.cpp
+++ GleeBug/Debugger.Process.cpp
@@ -1,10 +1,6 @@
 #include "Debugger.Process.h"
 #include "Debugger.Thread.Registers.h"
-
-#define ZYDIS_EXPORTS
-#define ZYDIS_ENABLE_FEATURE_IMPLICITLY_USED_REGISTERS
-#define ZYDIS_ENABLE_FEATURE_AFFECTED_FLAGS
-#include <Zydis/Zydis.h>
+#include "Zydis/Zydis.h"
 
 namespace GleeBug
 {
@@ -21,21 +17,45 @@ namespace GleeBug
             hardwareBreakpoints[i].internal.hardware.enabled = false;
     }
 
-    static bool IsRepeated(const ZydisInstructionInfo & info)
+    static bool IsRepeated(const ZydisDecodedInstruction & info)
     {
         // https://www.felixcloutier.com/x86/rep:repe:repz:repne:repnz
         // TODO: allow extracting the affected range
         switch(info.mnemonic)
         {
-        case ZYDIS_MNEMONIC_INS:
-        case ZYDIS_MNEMONIC_OUTS:
-        case ZYDIS_MNEMONIC_MOVS:
+        // INS
+        case ZYDIS_MNEMONIC_INSB:
+        case ZYDIS_MNEMONIC_INSW:
+        case ZYDIS_MNEMONIC_INSD:
+        // OUTS
+        case ZYDIS_MNEMONIC_OUTSB:
+        case ZYDIS_MNEMONIC_OUTSW:
+        case ZYDIS_MNEMONIC_OUTSD:
+        // MOVS
+        case ZYDIS_MNEMONIC_MOVSB:
+        case ZYDIS_MNEMONIC_MOVSW:
         case ZYDIS_MNEMONIC_MOVSD:
-        case ZYDIS_MNEMONIC_LODS:
-        case ZYDIS_MNEMONIC_STOS:
-        case ZYDIS_MNEMONIC_CMPS:
+        case ZYDIS_MNEMONIC_MOVSQ:
+        // LODS
+        case ZYDIS_MNEMONIC_LODSB:
+        case ZYDIS_MNEMONIC_LODSW:
+        case ZYDIS_MNEMONIC_LODSD:
+        case ZYDIS_MNEMONIC_LODSQ:
+        // STOS
+        case ZYDIS_MNEMONIC_STOSB:
+        case ZYDIS_MNEMONIC_STOSW:
+        case ZYDIS_MNEMONIC_STOSD:
+        case ZYDIS_MNEMONIC_STOSQ:
+        // CMPS
+        case ZYDIS_MNEMONIC_CMPSB:
+        case ZYDIS_MNEMONIC_CMPSW:
         case ZYDIS_MNEMONIC_CMPSD:
-        case ZYDIS_MNEMONIC_SCAS:
+        case ZYDIS_MNEMONIC_CMPSQ:
+        // SCAS
+        case ZYDIS_MNEMONIC_SCASB:
+        case ZYDIS_MNEMONIC_SCASW:
+        case ZYDIS_MNEMONIC_SCASD:
+        case ZYDIS_MNEMONIC_SCASQ:
             return (info.attributes & (ZYDIS_ATTRIB_HAS_REP | ZYDIS_ATTRIB_HAS_REPZ | ZYDIS_ATTRIB_HAS_REPNZ)) != 0;
         }
         return false;
@@ -47,14 +67,17 @@ namespace GleeBug
         unsigned char data[16];
         if(MemReadSafe(gip, data, sizeof(data)))
         {
-            ZydisInstructionInfo info;
-            memset(&info, 0, sizeof(info));
-            auto mode = GleeArchValue(ZYDIS_OPERATING_MODE_32BIT, ZYDIS_OPERATING_MODE_64BIT);
-            auto status = ZydisDecode(mode, data, sizeof(data), gip, &info);
-            auto stepOver = false;
-            if(ZYDIS_SUCCESS(status))
+            ZydisDisassembledInstruction instruction;
+            if(ZYAN_SUCCESS(ZydisDisassembleIntel(
+                                GleeArchValue(ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_MACHINE_MODE_LONG_COMPAT_32),
+                                gip,
+                                data,
+                                sizeof(data),
+                                &instruction
+                            )))
             {
-                switch(info.mnemonic)
+                bool stepOver = false;
+                switch(instruction.info.mnemonic)
                 {
                 case ZYDIS_MNEMONIC_CALL:
                 case ZYDIS_MNEMONIC_PUSHF:
@@ -63,11 +86,21 @@ namespace GleeBug
                     stepOver = true;
                     break;
                 default:
-                    stepOver = IsRepeated(info);
+                    stepOver = IsRepeated(instruction.info);
                     break;
                 }
+                if(stepOver)
+                {
+                    SetBreakpoint(gip + instruction.info.length, [cbStep](const BreakpointInfo & info)
+                    {
+                        cbStep();
+                    }, true, SoftwareType::ShortInt3);
+                    return;
+                }
             }
-            if(stepOver)
+        }
+        thread->StepInto(cbStep);
+    }
             {
                 SetBreakpoint(gip + info.length, [cbStep](const BreakpointInfo & info)
                 {
